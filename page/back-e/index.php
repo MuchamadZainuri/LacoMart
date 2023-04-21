1b4a357198fdaedd39d0aadb0f9ce481d3452ba0
Muchamad Zainuri <muchamadzainuri6@gmail.com>
2023-04-21 11:00:03 +0700

    beloman2

--- page/back-e/index.php
+++ page/back-e/index.php
@@ -196,8 +196,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="../../assets/script/scripts.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-    <script src="../../assets/demo/chart-area-demo.js"></script>
-    <script src="../../assets/demo/chart-bar-demo.js"></script>
+    <script src="../../assets/script/chart-area-demo.js"></script>
+    <script src="../../assets/script/chart-bar-demo.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
     <script src="../../assets/script/datatables-simple-demo.js"></script>
 </body>
